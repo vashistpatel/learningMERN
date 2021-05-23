b8bb00a89271020bffc81d8ba09f3d07816b59e0
Vashist Patel <vashist.patel@ontariotechu.net>
2021-05-23 13:07:31 -0400

    register users using axios.(35) change it to redux later

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState } from 'react';
+import axios from 'axios';
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -8,13 +9,33 @@ const Register = () => {
         password2:''
     });
     const { name, email, password, password2 } = formData;
+
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
-    const onSubmit = e => {
+
+    const onSubmit = async (e) => {
         e.preventDefault();
         if(password !== password2){
             console.log('passwords do not match');
         } else{
-            console.log(formData);
+            const newUser = {
+                name,
+                email,
+                password
+            }
+            try {
+                const config = {
+                    headers:{
+                        'Content-Type': 'application/json'
+                    }
+                }
+                const body = JSON.stringify(newUser);
+                const res = await axios.post('/api/users', body, config);
+                
+                console.log(res.data);
+
+            } catch (err) {
+                console.error(err.response.data);
+            }
         }
     }
     
