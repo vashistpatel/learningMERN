13b3cbfc5b235307bce305fa9ab58004250aa422
Vashist Patel <vashist.patel@ontariotechu.net>
2021-05-30 14:00:51 -0400

    added education and experince

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,7 +3,12 @@ import { setAlert } from './alert';
 
 import {
     GET_PROFILE,
-    PROFILE_ERROR
+    GET_PROFILES,
+    PROFILE_ERROR,
+    UPDATE_PROFILE,
+    CLEAR_PROFILE,
+    ACCOUNT_DELETED,
+    GET_REPOS
 } from './types';
 
 //get current users profile
@@ -46,6 +51,72 @@ export const createProfile = (formData, history, edit = false) => async dispatch
         history.push('/dashboard');
       }
           
+    } catch (err) {
+      const errors = err.response.data.errors;
+  
+      if (errors) {
+        errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+      }
+  
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+};
+
+// add experince
+export const addExperience = (formData, history) => async dispatch => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+  
+      const res = await axios.put('/api/profile/experience', formData, config);
+  
+      dispatch({
+        type: UPDATE_PROFILE,
+        payload: res.data
+      });
+  
+      dispatch(setAlert('Experience Added', 'success'));
+  
+      history.push('/dashboard');
+    } catch (err) {
+      const errors = err.response.data.errors;
+  
+      if (errors) {
+        errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+      }
+  
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+};
+  
+// Add Education
+export const addEducation = (formData, history) => async dispatch => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+  
+      const res = await axios.put('/api/profile/education', formData, config);
+  
+      dispatch({
+        type: UPDATE_PROFILE,
+        payload: res.data
+      });
+  
+      dispatch(setAlert('Education Added', 'success'));
+  
+      history.push('/dashboard');
     } catch (err) {
       const errors = err.response.data.errors;
   
